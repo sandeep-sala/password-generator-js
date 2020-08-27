809ead1730bcc8e2224d6cd9fa615544f81b4816
Sandeep Sala <sandeepsala@outlook.com>
2020-08-27 19:44:36 +0530

    temp bug solved

---
+++ README.md
@@ -0,0 +1,23 @@
+# Word Counter
+
+It is an Word Counter which count number of words ,letter and also give count of repeated word.
+
+## Installation
+
+```bash
+git clone https://github.com/sandeep-sala/word-counter.git
+or
+git clone https://github.com/sandeep-sala/word-counter.git <folder name>
+```
+
+
+## Execute
+
+```
+open index.html in browser
+```
+
+
+## Screenshots
+
+![ScreenShot](https://i.postimg.cc/d1hkYpK3/Annotation-2020-06-07-154204.png)
---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+
+
+
+function generatrePass(){
+    var c_l = document.getElementById('pass_length').value
+    var e = document.getElementById('in_symbol').value
+    var n = document.getElementById('in_number').value
+    var c = document.getElementById('in_LC').value
+    var s = document.getElementById('in_UC').value
+    var a = document.getElementById('in_AC').value
+    console.log(makePass(c_l,c,s,n,e,a))
+    
+}
+
+function makePass(length,c,s,n,e,a) {
+    var result     = '';
+    var o_charL    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var o_charS    = 'abcdefghijklmnopqrstuvwxyz';
+    var o_num      = '0123456789';
+    var o_spe      = '!@#$%^&*|?';
+    var o_abm      = '{}[]()\/"`~,;:.<>'+"'";
+    var char       = '';
+    if(c == "true"){ char+= o_charL }
+    if(s == "true"){ char+= o_charS }
+    if(n == "true"){ char+= o_num }
+    if(e == "true"){ char+= o_spe }
+    if(a == "true"){ char+= o_abm }
+
+    var charactersLength = char.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += char.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
