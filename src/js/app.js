c9f60bca1b87a7bb977bb9051ce684cda59e8557
Sandeep Sala <62003186+sandeep-sala@users.noreply.github.com>
2020-09-02 15:50:33 +0530

    bug fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,14 @@
 
 function generatrePass(){
     var c_l = document.getElementById('pass_length').value
-    var e = document.getElementById('in_symbol').value
-    var n = document.getElementById('in_number').value
-    var c = document.getElementById('in_LC').value
-    var s = document.getElementById('in_UC').value
-    var a = document.getElementById('in_AC').value
+    var e = document.getElementById('in_symbol').checked
+    var n = document.getElementById('in_number').checked
+    var s = document.getElementById('in_LC').checked
+    var c = document.getElementById('in_UC').checked
+    var a = document.getElementById('in_AC').checked
     console.log(makePass(c_l,c,s,n,e,a))
-    
+    document.getElementById('password-genratered').value = makePass(c_l,c,s,n,e,a)
+  
 }
 
 function makePass(length,c,s,n,e,a) {
@@ -20,11 +21,11 @@ function makePass(length,c,s,n,e,a) {
     var o_spe      = '!@#$%^&*|?';
     var o_abm      = '{}[]()\/"`~,;:.<>'+"'";
     var char       = '';
-    if(c == "true"){ char+= o_charL }
-    if(s == "true"){ char+= o_charS }
-    if(n == "true"){ char+= o_num }
-    if(e == "true"){ char+= o_spe }
-    if(a == "true"){ char+= o_abm }
+    if(c == true){ char+= o_charL }
+    if(s == true){ char+= o_charS }
+    if(n == true){ char+= o_num }
+    if(e == true){ char+= o_spe }
+    if(a == true){ char+= o_abm }
 
     var charactersLength = char.length;
     for ( var i = 0; i < length; i++ ) {
